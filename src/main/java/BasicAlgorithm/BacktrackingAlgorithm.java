2ae92e391be9cb7b8dc4af3962e00ad9b4dbe7b2
robert <robert.ljx.de.simon@gmail.com>
2023-02-17 23:59:51 +1100

    backtracking

---
+++ src/main/java/BasicAlgorithm/BacktrackingAlgorithm.java
@@ -0,0 +1,14 @@
+package BasicAlgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BacktrackingAlgorithm {
+    public List<List<Integer>> generatePermutation(List<Integer> elements){
+        List<List<Integer>> permutations=new ArrayList<>();
+
+    }
+    private void backtrack(List<List<Integer>> permutations, List<Integer> tempList, List<Integer> elements){
+
+    }
+}
