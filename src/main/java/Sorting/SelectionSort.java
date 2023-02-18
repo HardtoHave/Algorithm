110d04e21d3630ac46e2d526da33518a7ed60626
robert <robert.ljx.de.simon@gmail.com>
2023-02-18 17:37:29 +1100

    selection

---
+++ src/main/java/Sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        selectionSort(arr);
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            // swap arr[i] and arr[minIdx]
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+
+}
