917372d929f1036da5a9a9308fffd28387987ea7
robert <robert.ljx.de.simon@gmail.com>
2023-03-04 17:45:41 +1100

    graph

---
+++ src/main/java/Graph/Graph.java
@@ -0,0 +1,18 @@
+package Graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Graph {
+    public HashMap<Integer,Node> nodes;
+    public HashSet<Edge> edges;
+
+    public Graph(HashMap<Integer, Node> nodes, HashSet<Edge> edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public Graph() {
+
+    }
+}
